1c6ac9cba0db9e553cdd1fc0a182a1ad2470963c
Nome Sobrenome <carlos_danielbatista@hotmail.com>
2020-04-22 20:48:19 -0300

    first commit Aula16
    > first commit

---
+++ src/app/edicao/edicao.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Postagem } from '../models/postagem.model';
+import { PostagensService } from '../services/postagens.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-edicao',
+  templateUrl: './edicao.component.html',
+  styleUrls: ['./edicao.component.scss']
+})
+export class EdicaoComponent implements OnInit {
+
+  postagem: Postagem;
+
+  formulario = new FormGroup({
+    conteudo: new FormControl(null, [Validators.required]),
+  });
+
+  constructor(
+    private postagensService: PostagensService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+ async ngOnInit() {
+    const idPostagem = this.activatedRoute.snapshot.params.id;
+    this.postagem = await this.postagensService.retornarPostagem(idPostagem);
+    this.formulario.controls['conteudo'].setValue(this.postagem.conteudo);
+  }
+
+  async enviar() {
+
+    if (this.formulario.invalid) {
+      return;
+    }
+
+    // let postagem: Postagem = this.formulario.value;
+    this.postagem.conteudo = this.formulario.controls['conteudo'].value;
+    this.postagem.dataEdicao = new Date();
+    
+
+    this.postagem = await this.postagensService.atualizar(this.postagem);
+
+    this.router.navigate(['home']);
+
+  }
+
+}
---
+++ src/app/services/postagens.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Postagem } from '../models/postagem.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagensService {
+
+  produtos: string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  async salvar(novaPostagem: Postagem): Promise<Postagem> {
+    return this.http.post<Postagem>('http://localhost:3000/postagens', novaPostagem).toPromise();
+  }
+
+  async apagar(id: number): Promise<void> {
+    return this.http.delete<void>('http://localhost:3000' + id).toPromise();
+  }
+
+  async retornarPostagens(): Promise<Postagem[]> {
+    return this.http.get<Postagem[]>('http://localhost:3000/postagens').toPromise();
+  }
+
+  async atualizar(postagem: Postagem): Promise<Postagem> {
+    return this.http.put<Postagem>('http://localhost:3000/postagens' + postagem.id, postagem).toPromise();
+  }
+
+  async retornarPostagem(id: number): Promise<Postagem> {
+    return this.http.get<Postagem>('http://localhost:3000/postagens/' + id).toPromise();
+  }
+
+}
